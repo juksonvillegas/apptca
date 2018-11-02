a1ff912b70e6ed90f1b61b84d1223cd7fa9670b7
juksonvillegas <juksonvillegas@gmail.com>
2018-11-01 19:07:45 -0500

    se agrego modal para agregar cliente al 70%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarUserComponent } from './components/shared/navbar-user/navbar-user.component';
+import { ClientesComponent } from './components/clientes/clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarUserComponent
+    NavbarUserComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
